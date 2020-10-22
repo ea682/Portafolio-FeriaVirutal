0ba7df3784894d261eae41478f91e70cda703899
ea682 <47906508+ea682@users.noreply.github.com>
2020-10-22 18:47:04 -0300

    Se cambia la ruta de la API.

--- app/src/main/java/com/example/feriavirtual/API/Generales/config.java
+++ app/src/main/java/com/example/feriavirtual/API/Generales/config.java
@@ -1,5 +1,5 @@
 package com.example.feriavirtual.API.Generales;
 
 public class config {
-    public static final String ruta = "http://localhost:56664/api/";
+    public static final String ruta = "http://192.168.8.100/SX.Servicio.Generico/api/";
 }
